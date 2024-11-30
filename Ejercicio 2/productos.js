49e6fd45bc2c5bce977ff3bd7209042370cb221c
tiago <99084072+santa2150@users.noreply.github.com>
2024-11-30 10:10:24 -0500

    Entrega del Ejercicio2 - 55823512

---
+++ Ejercicio 2/productos.js
@@ -0,0 +1,15 @@
+const nombreProducto = document.getElementById("name").value;
+const cartegoria = document.getElementById("cartegoria").value;
+const precio = parseFloat(document.getElementById("precio").value);
+const unidades  = parseInt(document.getElementById("unidades").value);
+let descuento  = 0;
+
+if (cartegoria == 'A'){
+    switch (unidades){
+        case (1>=10) {
+
+        }
+    }
+}
+    let precioTotal = precio * unidades;
+    let valorDescuento = precioTotal - descuento
